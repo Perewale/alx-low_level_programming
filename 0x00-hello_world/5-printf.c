49e3d2fc37714840fe376202203845ab34abaa86
Tapere <ceraphafrica@gmail.com>
2023-02-16 19:16:43 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Print a C program using printf
- *
- * return : 0
+ * main - A program that prints a line using printf function
+ * return : 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,/n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
